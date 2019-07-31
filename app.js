0aed78efae1dfee3ccfc30aefeaa36f9309af4b4
alohaisai <mm70aloha@gmail.com>
2019-07-31 14:03:49 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let array = [
+  [["apple","英語"],["りんご","甘い"]],
+  [["orange","英語"],["みかん","酸っぱい"]]
+];
+
+console.log(array[0]);
+console.log(array[0][1]);
+console.log(array[1][1][1]);
+console.log(array[1][1][0]);
+
+array = [
+  ["pink","ピンク"],
+  ["red","赤"]
+];
+
+console.log(array[0]);
